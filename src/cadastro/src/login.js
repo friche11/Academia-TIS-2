5c84af07888d9c979111c3d718bee3d4092fc316
luanaf4 <luana.fleury4@gmail.com>
2023-04-23 02:27:23 -0300

    Atualização página de login #48

---
+++ src/cadastro/src/login.js
@@ -0,0 +1,42 @@
+import './App.css';
+import {Formik, form, field, ErrorMessage} from "formik"; //biblioteca para validação de dados
+import * as yup from "yup"; //validação de campos
+
+function App() {
+
+  const handleClickLogin = (value) => console.log(values);
+
+  const validationLogin = yup.object().shape({
+    email: yup.string().email().required("Este campo é obrigatório"),
+    password: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres").required("Este campo é obrigatório"),
+  });
+
+  return (
+    <div className="container">
+      //inicio formulario de login
+      <h1>Login</h1>
+      <Formik
+        initalValues={{}} onSubmit={handleClickLogin}
+        validationSchema={validationLogin}
+      >
+        <Form className="login-form">
+        
+        <div className="login-form-group">
+            <Filed name="email" className="form-field" placeholder="Email"></Filed>
+            <ErrorMessage component="span" name="email" className="form-error"></ErrorMessage>
+        </div>
+
+        <div className="login-form-group">
+            <Filed name="password" className="form-field" placeholder="Senha"></Filed>
+            <ErrorMessage component="span" name="password" className="form-error"></ErrorMessage>
+        </div>
+        
+        <button className="button" type="submit"></button>
+
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
