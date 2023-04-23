167c5e8e7911726acd8f858917bebb44389df4f5
luanaf4 <luana.fleury4@gmail.com>
2023-04-23 17:10:57 -0300

    Atualização formulário de Cadastro #47

--- src/cadastro/src/App.js
+++ src/cadastro/src/App.js
@@ -4,12 +4,21 @@ import * as yup from "yup"; //validação de campos
 
 function App() {
 
+  const handleClickRegister = (values) => console.log(values)
+
   const handleClickLogin = (values) => console.log(values);
 
   const validationLogin = yup.object().shape({
     email: yup.string().email().required("Este campo é obrigatório"),
     password: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres").required("Este campo é obrigatório"),
   });
+
+  const validationRegister = yup.object().shape({
+    email: yup.string().email().required("Este campo é obrigatório"),
+    password: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres").required("Este campo é obrigatório"),
+    confirmpassword: yup.string().min(8, "As senhas não coincidem").required("As senhas não coincidem"),
+  });
+
 
   return (
     <div className="container">
@@ -36,8 +45,8 @@ function App() {
       </Formik>
       <h1>Cadastro</h1>
       <Formik
-        initalValues={{}} onSubmit={handleClickLogin}
-        validationSchema={validationLogin}
+        initalValues={{}} onSubmit={handleClickRegister}
+        validationSchema={validationRegister}
       >
         <Form className="login-form">
         
@@ -50,8 +59,13 @@ function App() {
             <Field name="password" className="form-field" placeholder="Senha"></Field>
             <ErrorMessage component="span" name="password" className="form-error"></ErrorMessage>
         </div>
+
+        <div className="login-form-group">
+            <Field name="confirmpassword" className="form-field" placeholder="Confirme sua senha"></Field>
+            <ErrorMessage component="span" name="confirmpassword" className="form-error"></ErrorMessage>
+        </div>
         
-        <button className="button" type="submit">Login</button>
+        <button className="button" type="submit">Cadastre-se</button>
 
         </Form>
       </Formik>
