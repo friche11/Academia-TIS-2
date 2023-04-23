381f8376952fde87b21fd3ef2b200006fd9f3522
luanaf4 <luana.fleury4@gmail.com>
2023-04-23 01:42:43 -0300

    Criação formulario de login #48

--- src/cadastro/src/App.js
+++ src/cadastro/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import {formik, form, field, ErrorMessage} from "formik"; //biblioteca para validação de dados
+import * as yup from "yup"; //validação de campos
 
 function App() {
   return (
     <div className="container">
+      //inicio formulario de login
+      <h1>Login</h1>
+      <Formik
+        initalValues={{}}
+      >
+        <Form className="login-form">
+        
+        <div className="login-form-group">
+            <Filed name="email" className="form-field" placeholder="Email"></Filed>
+            <ErrorMessage component="span" name="email" className="form-error"></ErrorMessage>
+        </div>
+
+        </Form>
+      </Formik>
     </div>
   );
 }
