3a120693de6a45fcf2654ae5ca6493a0c94e7aab
luanaf4 <luana.fleury4@gmail.com>
2023-05-03 19:48:12 -0300

    Criação da pasta components #61

--- Client/src/Pages/PaginaInicial.js
+++ Client/src/Pages/PaginaInicial.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
+// utilizar 'Link' para carregar as páginas sem a necessidade de um novo pedido de HTTP
+
 function PaginaInicial() {
     return (
     <>
     <h1>Página Incial</h1>
-    <p>Go to <a href="/Login">Login</a>.</p>
-    <p>Go to <a href="/Login">Cadastro</a>.</p>
+    <p>Go to <Link to="/Login">Login</Link>.</p>
+    <p>Go to <Link to="/Cadastro">Cadastro</Link>.</p>
     </>
     );
 }
