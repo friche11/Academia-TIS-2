4d7d46ea762e2ef6877dd8046a356273c7fffc41
luanaf4 <luana.fleury4@gmail.com>
2023-05-03 19:33:50 -0300

    Criação da página inicial #60

---
+++ Client/src/Pages/PaginaInicial.js
@@ -0,0 +1,11 @@
+function PaginaInicial() {
+    return (
+    <>
+    <h1>Página Incial</h1>
+    <p>Go to <a href="/Login">Login</a>.</p>
+    <p>Go to <a href="/Login">Cadastro</a>.</p>
+    </>
+    );
+}
+
+export default PaginaInicial;
