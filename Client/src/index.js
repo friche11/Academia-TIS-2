44a2527e57414cbe07d02e024d41cae2ec468c50
luanaf4 <luana.fleury4@gmail.com>
2023-05-15 09:45:18 -0300

    Criação pasta env #57

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Sequelize } from "sequelize";
 
 import Rotas from "./Routes/routes"
 
 
 ReactDOM.render(<Rotas />, document.getElementById("root"));
 
+const sequelize = new Sequelize(
+    
+)
 
