3e37791d6c06648826baf3226fa7a1ac2ddd52a9
Victor Friche <vicfriche@gmail.com>
2023-06-13 19:12:03 -0300

    Configuracoes do projeto

---
+++ ProjetoSemReact/app.js
@@ -0,0 +1,18 @@
+// Carregando Modulos
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express()
+// Configuracoes
+
+
+
+// Rotas
+
+
+
+// Outros
+const PORT = 8081
+app.listen(PORT, () =>{
+    console.log("Servidor rodando!")
+})
