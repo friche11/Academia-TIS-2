71b75232e83d34a37c432c4131c86f2c41faae78
Victor Friche <vicfriche@gmail.com>
2023-06-23 09:56:48 -0300

    Correcao do login

--- ProjetoSemReact/config/auth.js
+++ ProjetoSemReact/config/auth.js
@@ -2,6 +2,10 @@ const localStrategy = require("passport-local").Strategy;
 const mysql = require("mysql2/promise");
 const bcrypt = require("bcryptjs");
 
+const db = require('../models/db.js')
+const Aluno = require('../models/Aluno.js')
+const Personal = require('../models/Personal.js')
+
 // Configurar a conexão com o banco de dados MySQL
 const connection = mysql.createPool({
   host: 'localhost',
@@ -13,20 +17,19 @@ const connection = mysql.createPool({
 module.exports = function(passport) {
   passport.use(
     new localStrategy({ usernameField: 'email', passwordField: 'senha' }, (email, senha, done) => {
-      connection.execute('SELECT * FROM alunos WHERE email = ?', [email])
-        .then(([rows, fields]) => {
-          if (!rows.length) {
+      Aluno.findOne({where: {email: email } }).then((aluno) => {
+          if (!aluno) {
+            console.log("erro")
             return done(null, false, { message: 'Essa conta não existe' });
           }
 
-          const alunos = rows[0];
-          bcrypt.compare(senha, alunos.senha, (erro, batem) => {
-            if (batem) {
-              return done(null, alunos);
+          const res = bcrypt.compare(senha, aluno.senha, (err, resposta) => {
+            if (resposta) {
+              return done(null, aluno);
             } else {
-              return done(null, false, { message: 'Senha incorreta' });
+              return done(null, false, { message: 'Senha incorreta'});
             }
-          });
+          })
         })
         .catch((err) => {
           return done(err);
@@ -35,20 +38,57 @@ module.exports = function(passport) {
   );
 
   // Passar dados do usuário autenticado para uma sessão
-  passport.serializeUser((alunos, done) => {
-    done(null, alunos.id);
+  passport.serializeUser((aluno, done) => {
+    done(null, aluno.id);
   });
 
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const [rows, fields] = await connection.execute('SELECT * FROM alunos WHERE id = ?', [id]);
-      if (!rows.length) {
-        return done(null, false, { message: 'Aluno não encontrado' });
+  passport.deserializeUser( (id, done) => {
+    Aluno.findOne({where: {id: id}}).then((res)=>{
+      if(res){
+        return done(null, false, {message: 'Usuario nao encontrado'})
+      }else{
+        return done(null, res)
       }
-      const alunos = rows[0];
-      done(null, alunos);
-    } catch (err) {
-      done(err, null);
-    }
+    })
   });
 };
+
+module.exports = function(passport) {
+  passport.use(
+    new localStrategy({ usernameField: 'email', passwordField: 'senha' }, (email, senha, done) => {
+      Personal.findOne({where: {email: email } }).then((personal) => {
+          if (!personal) {
+            console.log("erro")
+            return done(null, false, { message: 'Essa conta não existe' });
+          }
+
+          const res = bcrypt.compare(senha, personal.senha, (err, resposta) => {
+            if (resposta) {
+              return done(null, personal);
+            } else {
+              return done(null, false, { message: 'Senha incorreta'});
+            }
+          })
+        })
+        .catch((err) => {
+          return done(err);
+        });
+    })
+  );
+
+  // Passar dados do usuário autenticado para uma sessão
+  passport.serializeUser((personal, done) => {
+    done(null, personal.id);
+  });
+
+  passport.deserializeUser( (id, done) => {
+    Personal.findOne({where: {id: id}}).then((res)=>{
+      if(res){
+        return done(null, false, {message: 'Usuario nao encontrado'})
+      }else{
+        return done(null, res)
+      }
+    })
+  });
+};
+
